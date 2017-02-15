5aeb6a6526dafe0952922b00039ff2dbc1ce842e
Gerald Aqui <gaqui@spartaglobal.co>
2017-02-15 16:53:16 +0000

    created app.js, npm init, installed express

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+
+app.listen(port, function(){
+	console.log("The server is on and is listening on port " + port);
+});
