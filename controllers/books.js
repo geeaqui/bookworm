6f3ae2580c4ca599d34a5e853d8c78b2c7073e0e
Gerald Aqui <gaqui@spartaglobal.co>
2017-02-15 17:39:05 +0000

    added res.send() to each api functions inside controllers/books.js file

--- controllers/books.js
+++ controllers/books.js
@@ -1,29 +1,29 @@
 function indexBooks(req, res){
-
+	res.send('Index');
 }
 
 function showBook(req, res){
-
+	res.send('Show');
 }
 
 function editBook(req, res){
-
+	res.send('Edit');
 }
 
 function newBook(req, res){
-
+	res.send('New');
 }
 
 function createBook(req, res){
-
+	res.send('Create');
 }
 
 function updateBook(req, res){
-
+	res.send('Update');
 }
 
 function deleteBook(req, res){
-
+	res.send('Delete');
 }
 
 
