bb85fefdca07cd9489ce76633b8176bf8eea6961
Gerald Aqui <gaqui@spartaglobal.co>
2017-02-16 22:54:40 +0000

    ammended user controller and added a middleware for restricting url's in app.js file

--- controllers/users.js
+++ controllers/users.js
@@ -1,40 +1,29 @@
-// NEW - GET /new
-function newUser(req , res) {
+// NEW ( AKA Registration )
+function newUser(req,res) {
 
-  // create an empty user
-  var newUser = {
-    id: "",
-    title: "",
-    body: ""
-  }
+  res.render('users/new' , {title:"Register"});
 
-  res.render("posts/new" , {
-    title: "Register",
-    user: newUser
-  });
 }
 
-// CREATE - POST /
-function createUser(req , res) {
+// CREATE - Handles registrations
+function createUser(req,res){
 
-  // data is gathered by body parser and placed in req.body
+  // save the user
+  var user = new User(req.body);
 
-  // ask mongoose to save the data for us and wait for the response
-  User.create( req.body , function(err, post){
-  
+  user.save(function(err,user){
+
     // check for errors and return 500 if there was a problem
     if(err) req.flash('error' , err.message);
-  
-    // redirect the user to a GET route. We'll go back to the INDEX.
-   res.redirect("/");
-  
+
+    // redirect to the posts index page
+    res.redirect("/");
+
   });
+
 }
 
-// export all our controller functions in an object
 module.exports = {
-
   new: newUser,
   create: createUser
-
 }
